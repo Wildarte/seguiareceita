5fa10502c4668b204d47d2b9467b2bab1da6cb1f
wildarte <wildarte10@gmail.com>
2022-03-29 21:11:05 -0300

    estrutura da pagina home

---
+++ index.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <header>
+        <div>
+            <div>
+                <div>
+                    <img src="" alt="">
+                </div>
+                <div>
+                    <form action="">
+                        <input type="search" name="" id="">
+                        <input type="submit" value="">
+                    </form>
+                </div>
+
+                <div>
+                    <a href=""></a>
+                    <a href=""></a>
+                    <a href=""></a>
+                </div>
+            </div>
+            <div>
+                <ul>
+                    <li><a href="#"></a></li>
+                    <li><a href="#"></a></li>
+                    <li><a href="#"></a></li>
+                    <li><a href="#"></a></li>
+                    <li><a href="#"></a></li>
+                </ul>
+            </div>
+        </div>
+    </header>
+
+    <main>
+
+        <section>
+            <div>
+
+            </div>
+        </section>
+
+        <section>
+            <div>
+                <ul>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                </ul>
+            </div>
+
+        </section>
+
+        <section>
+
+            <header>
+                <h2>Destaque</h2>
+            </header>
+            
+            <section>
+                
+                <section>
+                    <article></article>
+                    <article></article>
+                    <article></article>
+                    <article></article>
+                    <article></article>
+                </section>
+
+                <aside>
+
+                </aside>
+
+            </section>
+
+
+            <section>
+                <section>
+                    <header>
+                        <h2>Últimas publicações</h2>
+                    </header>
+
+                    <section>
+                        <article></article>
+                        <article></article>
+                        <article></article>
+                        <article></article>
+                        <article></article>
+                        <article></article>
+                    </section>
+                </section>
+                
+                <aside></aside>
+
+            </section>
+
+        </section>
+
+    </main>
+
+
+    <footer>
+        <div>
+            <div>
+                <img src="" alt="">
+            </div>
+
+            <div>
+                <ul>
+                    <li><a href="#"></a></li>
+                    <li><a href="#"></a></li>
+                    <li><a href="#"></a></li>
+                    <li><a href="#"></a></li>
+                </ul>
+            </div>
+
+            <div>
+                <div>
+                    <h4>Contato</h4>
+
+                    <ul>
+                        <li><a href="#"></a></li>
+                        <li><a href="#"></a></li>
+                        <li><a href="#"></a></li>
+                    </ul>
+                </div>                
+                
+                <div>
+                    <h3>Redes sociais</h3>
+
+                    <ul>
+                        <li><a href="#"></a></li>
+                        <li><a href="#"></a></li>
+                        <li><a href="#"></a></li>
+                    </ul>
+                </div>
+
+                
+            </div>
+        </div>
+        <div>
+            <p>seguieareceita.com</p>
+        </div>
+    </footer>
+    
+</body>
+</html>
