6c7c47553bd2c58de762a38da7d4b2c1703cee13
wildarte <wildarte10@gmail.com>
2022-03-31 22:57:34 -0300

    parte de cima do header

--- index.php
+++ index.php
@@ -4,28 +4,35 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="assets/bootstrap-icons-1.8.1/bootstrap-icons.css">
+    <link rel="stylesheet" href="assets/css/reset.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Segui a Receita</title>
 </head>
 <body>
 
-    <header>
-        <div>
-            <div>
-                <div>
-                    <img src="" alt="">
-                </div>
-                <div>
-                    <form action="">
-                        <input type="search" name="" id="">
-                        <input type="submit" value="">
-                    </form>
-                </div>
+    <header class="header">
+        <div class="header_content container_full">
+            <div class="header_top">
+                <div class="header_top_content container">
+                    <div class="header_logo">
+                        <a href="#">
+                            <img src="assets/img/logo-small.png" alt="">
+                        </a>
+                    </div>
+                    <div class="header_form">
+                        <form action="">
+                            <input type="search" name="" id="" placeholder="buscar receitas...">
+                            <button type="submit"><i class="bi bi-search"></i></button>
+                        </form>
+                    </div>
 
-                <div>
-                    <a href=""></a>
-                    <a href=""></a>
-                    <a href=""></a>
+                    <div class="header_social">
+                        <a href="#"><i class="bi bi-facebook"></i></a>
+                        <a href="#"><i class="bi bi-instagram"></i></a>
+                    </div>
                 </div>
+                
             </div>
             <div>
                 <ul>
