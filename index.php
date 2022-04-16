b28ee78e8825c29d029b4a8ba0249d1ca185cc77
wildarte <wildarte10@gmail.com>
2022-04-16 19:28:42 -0300

    terminei a versao desktop

--- index.php
+++ index.php
@@ -78,107 +78,304 @@
             </div>
         </section>
 
-        <section>
-            <div>
-                <ul>
-                    <li><a href=""></a></li>
-                    <li><a href=""></a></li>
-                    <li><a href=""></a></li>
-                    <li><a href=""></a></li>
+        <section class="section_second_menu container_full">
+            <div class="content_second_menu container">
+                <ul class="second_menu">
+                    <li><a href=""><img src="assets/img/icons/cake-icon.png" alt=""> Bolos</a></li>
+                    <li><a href=""><img src="assets/img/icons/lanche-icon.png" alt=""> Lanches</a></li>
+                    <li><a href=""><img src="assets/img/icons/massa-icon.png" alt=""> Massas</a></li>
+                    <li><a href=""><img src="assets/img/icons/torta-icon.png" alt=""> Tortas</a></li>
+                    <li><a href=""><img src="assets/img/icons/sobremesa-icon.png" alt=""> Sobremesas</a></li>
+                    <li><a href=""><img src="assets/img/icons/mousse.png" alt=""> Mousses</a></li> 
                 </ul>
             </div>
 
         </section>
 
-        <section>
+        <section class="sec_content_one container_full">
 
-            <header>
-                <h2>Destaque</h2>
-            </header>
-            
-            <section>
-                
-                <section>
-                    <article></article>
-                    <article></article>
-                    <article></article>
-                    <article></article>
-                    <article></article>
+            <div class="content_one_content container">
+
+                <section class="content_one_left">
+
+                    <header class="header_content_one">
+                        <h2>Destaque</h2>
+                    </header>
+
+                    <section class="articles_content_one">
+                        <article class="card_content_one post_destaque">
+                            <div class="bord_icon_post">
+                                <img class="img_icon_post" src="assets/img/logo-small.png" alt="">
+                            </div>
+                            
+                            <a href="#">
+                                <img src="assets/img/posts/post1.jpg" alt="">
+                            </a>
+                            <h2>Pizza de calabreza caseira</h2>
+                        </article>
+
+                        <article class="card_content_one">
+                            <div class="bord_icon_post">
+                                <img class="img_icon_post" src="assets/img/logo-small.png" alt="">
+                            </div>
+                            <a href="#">
+                                <img src="assets/img/posts/post2.jpg" alt="">
+                            </a>
+                            
+                            <h2>Risoles</h2>
+                        </article>
+
+                        <article class="card_content_one">
+                            <div class="bord_icon_post">
+                                <img class="img_icon_post" src="assets/img/logo-small.png" alt="">
+                            </div>
+                            <a href="#">
+                                <img src="assets/img/posts/post3.jpg" alt="">
+                            </a>
+                            
+                            <h2>churrasco gringo</h2>
+                        </article>
+
+                        <article class="card_content_one">
+                            <div class="bord_icon_post">
+                                <img class="img_icon_post" src="assets/img/logo-small.png" alt="">
+                            </div>  
+                            <a href="#">
+                                <img src="assets/img/posts/post4.jpg" alt="">
+                            </a>
+                            
+                            <h2>salada gourme</h2>
+                        </article>
+                        
+
+                        <article class="card_content_one">
+                            <div class="bord_icon_post">
+                                <img class="img_icon_post" src="assets/img/logo-small.png" alt="">
+                            </div>
+                            <a href="#">
+                                <img src="assets/img/posts/post5.jpg" alt="">
+                            </a>
+                            <h2>Panqueca de queijo</h2>
+                        </article>
+
+                        
+
+                    </section>
+
                 </section>
 
-                <aside>
+                <aside class="sidebar_1">
+                    <form action="">
+                        <label for="Pesquisar">Pesquisar...</label>
+                        <input type="text" name="s">
+                    </form>
 
+                    <hr>
+
+                    <h3>Tags</h3>
+
+                    #bolos, #tortas, #recheio, #sobremesa, #sorvete, #salgado
+
+                    <a href="#">
+                        <img src="assets/img/ads.jpg" alt="">
+                    </a>
+
+                    <a href="#">
+                        <img src="assets/img/ads2.jpg" alt="">
+                    </a>
+                    
                 </aside>
 
-            </section>
+            </div>
 
+        </section>
 
-            <section>
-                <section>
-                    <header>
+        <section class="sec_separador container_full">
+            <div class="separador_content container">
+                <img src="assets/img/logo-small.png" alt="">
+            </div>
+        </section>
+
+        <section class="section_last_post container_full">
+
+            <div class="last_post_content container">
+
+                <div class="last_post_left">
+                    <header class="header_last_post">
                         <h2>Últimas publicações</h2>
                     </header>
 
-                    <section>
-                        <article></article>
-                        <article></article>
-                        <article></article>
-                        <article></article>
-                        <article></article>
-                        <article></article>
-                    </section>
-                </section>
-                
-                <aside></aside>
+                    <section class="posts_last_post">
+                        <article class="card_last_post">
+                            <a class="link_last_post" href="#">
+                                <img src="assets/img/posts/post9.jpg" alt="">
+                                <h3>churrasco com legumes</h3>
+                            </a>
+                            <div class="bottom_card_last_post">
+                                <a class="link_cat_last_post" href="#">comidas</a>
+                                <time>12/02/2022</time>
+                                
+                                <a class="link_excerpt_last_post" href="#">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat suscipit fugit soluta laboriosam neque</a>
+                                
+                            </div>
+                        </article>
+                        <article class="card_last_post">
+                            <a class="link_last_post" href="#">
+                                <img src="assets/img/posts/post8.jpg" alt="">
+                                <h3>churrasco com legumes</h3>
+                            </a>
+                            <div class="bottom_card_last_post">
+                                <a class="link_cat_last_post" href="#">comidas</a>
+                                <time>12/02/2022</time>
+                                
+                                <a class="link_excerpt_last_post" href="#">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat suscipit fugit soluta laboriosam neque</a>
+                                
+                            </div>
+                        </article>
+                        <article class="card_last_post">
+                            <a class="link_last_post" href="#">
+                                <img src="assets/img/posts/post7.jpg" alt="">
+                                <h3>churrasco com legumes</h3>
+                            </a>
+                            <div class="bottom_card_last_post">
+                                <a class="link_cat_last_post" href="#">comidas</a>
+                                <time>12/02/2022</time>
+                                
+                                <a class="link_excerpt_last_post" href="#">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat suscipit fugit soluta laboriosam neque</a>
+                                
+                            </div>
+                        </article>
+                        <article class="card_last_post">
+                            <a class="link_last_post" href="#">
+                                <img src="assets/img/posts/post6.jpg" alt="">
+                                <h3>churrasco com legumes</h3>
+                            </a>
+                            <div class="bottom_card_last_post">
+                                <a class="link_cat_last_post" href="#">comidas</a>
+                                <time>12/02/2022</time>
+                                
+                                <a class="link_excerpt_last_post" href="#">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima, doloribus! Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat suscipit fugit soluta laboriosam neque</a>
+                                
+                            </div>
+                        </article>
+                        <article class="card_last_post">
+                            <a class="link_last_post" href="#">
+                                <img src="assets/img/posts/post5.jpg" alt="">
+                                <h3>churrasco com legumes</h3>
+                            </a>
+                            <div class="bottom_card_last_post">
+                                <a class="link_cat_last_post" href="#">comidas</a>
+                                <time>12/02/2022</time>
+                                
+                                <a class="link_excerpt_last_post" href="#">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat suscipit fugit soluta laboriosam neque</a>
+                                
+                            </div>
+                        </article>
+                        <article class="card_last_post">
+                            <a class="link_last_post" href="#">
+                                <img src="assets/img/posts/post4.jpg" alt="">
+                                <h3>churrasco com legumes</h3>
+                            </a>
+                            <div class="bottom_card_last_post">
+                                <a class="link_cat_last_post" href="#">comidas</a>
+                                <time>12/02/2022</time>
+                                
+                                <a class="link_excerpt_last_post" href="#">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat suscipit fugit soluta laboriosam neque</a>
+                                
+                            </div>
+                        </article>
+                        <article class="card_last_post">
+                            <a class="link_last_post" href="#">
+                                <img src="assets/img/posts/post3.jpg" alt="">
+                                <h3>churrasco com legumes</h3>
+                            </a>
+                            <div class="bottom_card_last_post">
+                                <a class="link_cat_last_post" href="#">comidas</a>
+                                <time>12/02/2022</time>
+                                
+                                <a class="link_excerpt_last_post" href="#">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat suscipit fugit soluta laboriosam neque Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo quae facilis voluptatibus dignissimos nobis dolores nemo veniam, unde magnam?</a>
+                                
+                            </div>
+                        </article>
+                        <article class="card_last_post">
+                            <a class="link_last_post" href="#">
+                                <img src="assets/img/posts/post2.jpg" alt="">
+                                <h3>churrasco com legumes</h3>
+                            </a>
+                            <div class="bottom_card_last_post">
+                                <a class="link_cat_last_post" href="#">comidas</a>
+                                <time>12/02/2022</time>
+                                
+                                <a class="link_excerpt_last_post" href="#">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat suscipit fugit soluta laboriosam neque</a>
+                                
+                            </div>
+                        </article>
 
-            </section>
+                    </section>
+                </div>
+                
+                <aside class="sidebar_2">
+                    <a href="#">
+                        <img src="assets/img/ads3.jpg" alt="">
+                    </a>
+
+                    <a href="#">
+                        <img src="assets/img/ads4.jpg" alt="">
+                    </a>
+
+                    <a href="#">
+                        <img src="assets/img/ads5.jpg" alt="">
+                    </a>
+                </aside>
+
+            </div>
 
         </section>
 
     </main>
 
+    <footer class="footer container_full">
+        <div class="footer_content container">
 
-    <footer>
-        <div>
-            <div>
-                <img src="" alt="">
+            <div class="footer_logo">
+                <img src="assets/img/logo-small.png" alt="">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex nemo, cupiditate eveniet a autem qui alias numquam velit magni mollitia dolore ipsa, provident facilis. Corporis amet eius consequatur quos voluptatum.</p>
             </div>
 
-            <div>
+            <div class="footer_categories">
+                <h4>Categorias</h4>
                 <ul>
-                    <li><a href="#"></a></li>
-                    <li><a href="#"></a></li>
-                    <li><a href="#"></a></li>
-                    <li><a href="#"></a></li>
+                    <li><a href="#">Bolos</a></li>
+                    <li><a href="#">Doces</a></li>
+                    <li><a href="#">Salgados</a></li>
+                    <li><a href="#">Sobremesas</a></li>
                 </ul>
             </div>
 
-            <div>
-                <div>
+            <div class="footer_contato">
+                <div class="footer_contato_email">
                     <h4>Contato</h4>
 
                     <ul>
-                        <li><a href="#"></a></li>
-                        <li><a href="#"></a></li>
-                        <li><a href="#"></a></li>
+                        <li><a href="mailto:contato@dominio.com"><i class="bi bi-envelope"></i> contato@dominio.com</a></li>
                     </ul>
                 </div>                
                 
-                <div>
-                    <h3>Redes sociais</h3>
+                <div class="footer_social">
+                    <h4>Redes sociais</h4>
 
                     <ul>
-                        <li><a href="#"></a></li>
-                        <li><a href="#"></a></li>
-                        <li><a href="#"></a></li>
+                        <li><a href="#"><i class="bi bi-facebook"></i></a></li>
+                        <li><a href="#"><i class="bi bi-instagram"></i></a></li>
                     </ul>
                 </div>
 
                 
             </div>
         </div>
-        <div>
-            <p>seguieareceita.com</p>
+
+        <div class="footer_bottom">
+            <p><a href="#">seguieareceita.com</a></p>
         </div>
     </footer>
 
